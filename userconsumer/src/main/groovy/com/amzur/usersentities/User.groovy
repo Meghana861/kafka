56ead0555c0b618006b16048473de3373b06577a
Meghana861 <meghaanaas2002@gmail.com>
2024-08-28 19:21:09 +0530

    add

---
+++ userconsumer/src/main/groovy/com/amzur/usersentities/User.groovy
@@ -0,0 +1,13 @@
+package com.amzur.usersentities
+
+class User {
+    int id
+    String name
+    int age
+
+    User(int id, String name, int age){
+        this.id=id
+        this.name=name
+        this.age=age
+    }
+}
